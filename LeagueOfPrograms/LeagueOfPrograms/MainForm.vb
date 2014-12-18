ae39b6bf05fbbce9920bf34b75374e8fd4415014
ratminer <ratminer_54321@hotmail.com>
2014-12-18 00:42:18 -0500

    piiiiiiiiiiiiiiiiing
    and gitignore

--- LeagueOfPrograms/LeagueOfPrograms/MainForm.vb
+++ LeagueOfPrograms/LeagueOfPrograms/MainForm.vb
@@ -8,7 +8,7 @@ Public Class MainForm
     Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
         Dim pingSender As New Ping()
 
-        Dim reply As PingReply = pingSender.Send("216.52.241.254", 1000)
+        Dim reply As PingReply = pingSender.Send("50.203.115.149", 1000)
 
         ping.Text = "Ping: " & reply.RoundtripTime
         If reply.RoundtripTime > 200 Then
