fd3860d97ca41cf31f8bd299a4c5bfa535b6e0ed
seanD111 <sean_delong@hotmail.ca>
2014-12-17 19:06:21 -0500

    added project to git

---
+++ LeagueOfPrograms/LeagueOfPrograms/MainForm.Designer.vb
@@ -0,0 +1,59 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class MainForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.ping = New System.Windows.Forms.Label()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.SuspendLayout()
+        '
+        'ping
+        '
+        Me.ping.AutoSize = True
+        Me.ping.Location = New System.Drawing.Point(12, 9)
+        Me.ping.Name = "ping"
+        Me.ping.Size = New System.Drawing.Size(34, 13)
+        Me.ping.TabIndex = 0
+        Me.ping.Text = "Ping: "
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        Me.Timer1.Interval = 10000
+        '
+        'MainForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 262)
+        Me.Controls.Add(Me.ping)
+        Me.Name = "MainForm"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents ping As System.Windows.Forms.Label
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+
+End Class
---
+++ LeagueOfPrograms/LeagueOfPrograms/MainForm.vb
@@ -0,0 +1,24 @@
+﻿Imports System.Net.NetworkInformation
+Public Class MainForm
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+        Dim pingSender As New Ping()
+
+        Dim reply As PingReply = pingSender.Send("216.52.241.254", 1000)
+
+        ping.Text = "Ping: " & reply.RoundtripTime
+        If reply.RoundtripTime > 200 Then
+            ping.ForeColor = Color.Red
+        Else : ping.ForeColor = Color.Green
+        End If
+
+        If reply.RoundtripTime = 0 Then
+            ping.Text = "Ping: Checking..."
+        Else : ping.Text = "Ping: " & reply.RoundtripTime
+        End If
+    End Sub
+End Class
