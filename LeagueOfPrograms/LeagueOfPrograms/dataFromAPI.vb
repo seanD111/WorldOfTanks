75b355e2672b4bc03c2d4661b5bc26ddc2ffdb0c
seanD111 <sean_delong@hotmail.ca>
2014-12-19 04:01:05 -0500

    made changes to 'data' base class
    going to bed

--- LeagueOfPrograms/LeagueOfPrograms/dataFromAPI.vb
+++ LeagueOfPrograms/LeagueOfPrograms/dataFromAPI.vb
@@ -1,5 +1,8 @@
 ﻿Public Class dataFromAPI
     Protected fetchedData As String
-    Protected Overridable Sub requestData()
+
+    Protected Overridable Sub requestData(ByVal key As String)
+    End Sub
+    Protected Overridable Sub populateFields(ByVal key As String)
     End Sub
 End Class
